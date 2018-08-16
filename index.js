cae1faadb9963240fc2421ed06949ba05499f8f1
Ruben Taelman <ruben.taelman@ugent.be>
2018-08-16 14:17:11 +0200

    Fix null being pushed on initially empty array

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = function(array) {
   var readable = new (require('stream').Readable)({ objectMode: true });
   readable._read = function(size) {
-    readable.push(array.shift(size));
+    if (array.length) {
+      readable.push(array.shift(size));
+    }
     if (!array.length) {
       readable.push(null);
     }
